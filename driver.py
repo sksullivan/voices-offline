02a7d02e8405611686fce06a7eefba92da5dd988
Ubuntu <ubuntu@ip-172-31-16-137.us-west-2.compute.internal>
2017-01-30 06:36:32 +0000

    Initial victory

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 from audiolazy import *
 import numpy as np
 from scipy.signal import lfilter, hamming
@@ -6,14 +6,35 @@ import wave
 import math
 import sys
 
-def process(sound):
-# sound = wave.open(sys.argv[1],'r')
-	sample_freq = sound.getframerate()
-	raw_data = sound.readframes(-1)
-	raw_data = np.fromstring(raw_data,'Int16')
+sample_freq = 0
+samples = np.array([])
+target_sample_len = 65536
 
-	sample_len = len(raw_data)
-	windowed = raw_data * np.hamming(sample_len)
+def first_two(arr):
+	vals = []
+	i=0
+	while i < len(arr):
+		if len(vals) >= 2:
+			return vals
+		elif arr[i] != 0:
+			vals.append(arr[i])
+		i += 1
+
+
+
+def process(sound):
+	global samples
+	if len(samples) < target_sample_len:
+		samples = np.concatenate((samples,sound))
+		return
+	#sound = wave.open("eee.wav",'r')
+	#sample_freq = sound.getframerate()
+	#raw_data = sound.readframes(-1)
+	#raw_data = np.fromstring(raw_data,'Int16')
+	#print np.shape(raw_data)
+
+	sample_len = len(samples)
+	windowed = samples * np.hamming(sample_len)
 	high_passed = lfilter([1], [1., -0.63], windowed)
 
 	f = lpc(high_passed, order=2+sample_freq/1000)
@@ -21,7 +42,10 @@ def process(sound):
 	real_roots = roots[np.where(np.imag(roots)>=0)]
 	angles = np.arctan2(np.imag(real_roots),np.real(real_roots))
 	freqs = sorted(angles * (sample_freq / (2 * math.pi)))
-	print freqs
+
+	samples = np.array([])
+
+	return first_two(freqs)
 
 
 from flask import Flask
@@ -32,6 +56,7 @@ ws = GeventWebSocket(app)
 
 @ws.route('/audio')
 def audio(ws):
+   global sample_freq
    first_message = True
    total_msg = ""
    sample_rate = 0
@@ -40,14 +65,18 @@ def audio(ws):
       msg = ws.receive()
 
       if first_message and msg is not None: # the first message should be the sample rate
-         sample_rate = getSampleRate(msg)
+         sample_freq = int(msg.split(':')[1])
          first_message = False
          continue
       elif msg is not None:
-         audio_as_int_array = numpy.frombuffer(msg, 'i2')
-         doSomething(audio_as_int_array)
+	 print 'msg'
+	 audio_as_int_array = np.frombuffer(msg, 'i2')
+         formants = process(audio_as_int_array)
+	 if formants != None:
+		 ws.send(str(formants))
       else:
          break
 
+#process(None)
 if __name__ == '__main__':
-    app.run(gevent=100)
+    app.run(gevent=100,host='0.0.0.0')
